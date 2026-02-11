056bf965162c4e47e20a4a3ab824d03f6b4abe2c
Your Name <joaovictor8600@yahoo.com.br>
2026-02-11 14:02:58 -0300

    dominio

--- app/main.py
+++ app/main.py
@@ -21,10 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://zapbudget.com.br", 
-        "https://www.zapbudget.com.br",
         "https://zapbudget.vercel.app",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
